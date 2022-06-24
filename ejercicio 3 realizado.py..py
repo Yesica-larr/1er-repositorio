2f90df2760e5e807939439f135d46f5f3453d18d
Yesica-larr <102685358+Yesica-larr@users.noreply.github.com>
2022-06-24 08:48:42 -0300

    Add files via upload

---
+++ ejercicio 3 realizado.py..py
@@ -0,0 +1,16 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+¡HOLA MUNDO!
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+¡HOLA MUNDO!
+¡HOLAA Mundo!
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+¡HOLAA Mundo!
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+introduce tu nombre:yesica
+¡holayesica! 
---
+++ ejercicio 5 realizado.py..py
@@ -0,0 +1,34 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+¡HOLA MUNDO!
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+¡HOLA MUNDO!
+¡HOLAA Mundo!
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+¡HOLAA Mundo!
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+introduce tu nombre:yesica
+¡holayesica! 
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+introduce tu nombre: luciana
+False
+
+
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+introduce tu nombre: yesica
+False
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+0.25
+
+= RESTART: C:/Users/Laura/AppData/Local/Programs/Python/Python310/EJERCICIO 1 .py..py
+CUANTAS HORAS TRABAJAS: 9
+ ¿CUANTO TE PAGAN POR HORA?=200
+te pagan 1800.0
